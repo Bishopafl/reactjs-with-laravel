37b636f1cc9410c337948591d283def617515de1
Adam Lopez <adamf04@gmail.com>
2022-08-02 00:12:07 -0400

    Adding a top navigation bar and some links to our scaffold

---
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -0,0 +1,64 @@
+import React, { Component, Fragment } from 'react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+class TopNavigation extends Component {
+
+    constructor() {
+        super(); // call the super function when the component is loaded
+        /**
+         * This will show two different states for our nav bar
+         * a class `.navTitle` is made for the inital view
+         * 
+         * then when the document is scrolled, another class
+         * will load for that view `navTitleScroll`
+         */
+        this.state={
+            navBarTitle: "navTitle"
+        }
+    }
+
+    onScroll = () => {
+        if(window.scrollY > 100) {
+            this.setState({
+                navBarTitle: 'navTitleScroll'
+            })
+        } else if(window.scrollY < 100) {
+            this.setState({
+                navBarTitle: 'navTitle'
+            })
+        }
+    }
+
+    // this method will be executed when the component is run
+    componentDidMount() {
+        window.addEventListener('scroll', this.onScroll)
+    }
+
+    render() {
+        return (
+        <Fragment>
+            <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand className={this.state.navBarTitle} href="#home">Adam Lopez</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">HOME</Nav.Link>
+                        <Nav.Link href="#deets">ABOUT</Nav.Link>
+                        <Nav.Link href="#deets">SERVICE</Nav.Link>
+                        <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
+                        <Nav.Link href="#deets">ARTWORK</Nav.Link>
+                        <Nav.Link href="#deets">CONTACT US</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Fragment>
+        )
+    }
+}
+
+export default TopNavigation

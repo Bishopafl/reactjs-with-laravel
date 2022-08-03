66faff3ad5a93dec56c229038eb35db1fa1cff51
Adam Lopez <adamf04@gmail.com>
2022-08-03 13:49:58 -0400

    Got the summary component set up, there were some major tweaks to the original design that I think make the component look much more engaging. Going to implement a counter for the static numbers next

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,6 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import { Card, Col, Container, Row 
  } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import calendarIcon from '../../asset/image/calendar.gif';
+import networkIcon from '../../asset/image/network.gif';
+import waterIcon from '../../asset/image/water.gif';
 
 class Summary extends Component {
   render() {
@@ -13,18 +18,21 @@ class Summary extends Component {
                         <Col lg={8} md={6} sm={12}>
                             <Row className='countSection'>
                                 <Col>
+                                    <img className='summaryIcon' src={calendarIcon} />
                                     <h1 className='countNumber'>66,481</h1>
                                     <h4 className='countText'>Hours coding</h4>
                                     <hr className="text-light w-25 m-auto" />
                                 </Col>
                                 
                                 <Col>
+                                    <img className='summaryIcon' src={networkIcon} />
                                     <h1 className='countNumber'>77</h1>
                                     <h4 className='countText'>Github Repositories</h4>
                                     <hr className="text-light w-25 m-auto" />
                                 </Col>
 
                                 <Col>
+                                    <img className='summaryIcon' src={waterIcon} />
                                     <h1 className='countNumber'>8</h1>
                                     <h4 className='countText'>Cups of water daily</h4>
                                     <hr className="text-light w-25 m-auto" />
@@ -39,10 +47,11 @@ class Summary extends Component {
                                 <Card.Body>
                                     <Card.Title className='cardTitle'>What I Have Achieved</Card.Title>
                                     <Card.Text>
-                                    <p className="cardSubTitle text-start">Requirement Gathering</p>
-                                    <p className="cardSubTitle text-start">System Analysis</p>
-                                    <p className="cardSubTitle text-start">CI/CD Code Testing</p>
-                                    <p className="cardSubTitle text-start">Implementtation</p>
+                                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> Requirement Gathering</p>
+                                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> Requirement Gathering</p>
+                                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> System Analysis</p>
+                                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> CI/CD Code Testing</p>
+                                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> Implementtation</p>
                                     </Card.Text>
                                     
                                 </Card.Body>

59a6d92952fb0a0ebb9aa37809ab1bc63ada71ec
Adam Lopez <adamf04@gmail.com>
2022-08-03 11:50:11 -0400

    Card section within summary area is loaded with some text and styled. React has a different way to syntax bootstrap within templates, it's interesting. Will add some font awesome icons to the summary points in next commit.

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Col, Container, Row 
+import { Card, Col, Container, Row 
  } from 'react-bootstrap';
 
 class Summary extends Component {
@@ -8,26 +8,47 @@ class Summary extends Component {
       <Fragment>
         <Container fluid={true} className='summaryBanner p-0'>
             <div className='summaryBannerOverlay'>
-                <Container>
+                <Container className='text-center'>
                     <Row>
                         <Col lg={8} md={6} sm={12}>
-                            <Row>
+                            <Row className='countSection'>
                                 <Col>
-                                    <h1>One</h1>
+                                    <h1 className='countNumber'>66,481</h1>
+                                    <h4 className='countText'>Hours coding</h4>
+                                    <hr className="text-light w-25 m-auto" />
                                 </Col>
                                 
                                 <Col>
-                                    <h1>Two</h1>
+                                    <h1 className='countNumber'>77</h1>
+                                    <h4 className='countText'>Github Repositories</h4>
+                                    <hr className="text-light w-25 m-auto" />
                                 </Col>
 
                                 <Col>
-                                    <h1>Three</h1>
+                                    <h1 className='countNumber'>8</h1>
+                                    <h4 className='countText'>Cups of water daily</h4>
+                                    <hr className="text-light w-25 m-auto" />
                                 </Col>
                             </Row>
                         </Col>
 
                         <Col lg={4} md={6} sm={12}>
-                            <h1>Four</h1>
+                            
+
+                            <Card className='workCard'>
+                                <Card.Body>
+                                    <Card.Title className='cardTitle'>What I Have Achieved</Card.Title>
+                                    <Card.Text>
+                                    <p className="cardSubTitle text-start">Requirement Gathering</p>
+                                    <p className="cardSubTitle text-start">System Analysis</p>
+                                    <p className="cardSubTitle text-start">CI/CD Code Testing</p>
+                                    <p className="cardSubTitle text-start">Implementtation</p>
+                                    </Card.Text>
+                                    
+                                </Card.Body>
+                            </Card>
+
+
                         </Col>
                     </Row>
                 </Container>

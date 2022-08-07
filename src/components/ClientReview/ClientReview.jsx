2ad9337e20f93760e41be81de4843ad398d44b53
Adam Lopez <adamf04@gmail.com>
2022-08-06 22:24:52 -0400

    Completed the client review section. I'm not sure if I will merge this branch into master or not as it is not a component I am looking to implement on my site, however I might bring it just to have all components showing when I set up the backend API and then not display them later

--- src/components/ClientReview/ClientReview.jsx
+++ src/components/ClientReview/ClientReview.jsx
@@ -1,8 +1,54 @@
 import React, { Component, Fragment } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 class ClientReview extends Component {
   render() {
+    // only load one slider when component is rendered
+    var settings = {
+        autoPlay: true, // sets autoplay
+        autoPlaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        arrows: false,
+        speed: 3000,
+        vertical: true, // enables vertical slide
+        verticalSwiping: true, // enables vertical swipe
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              initialSlide: 1 
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
+
+
     return (
       <Fragment>
         <Container fluid={true} className="siderBack">
@@ -10,14 +56,37 @@ class ClientReview extends Component {
                 <h1 className='reviewMainTitle p-3'>Testimonials</h1>
                 <div className="reviewBottom "></div>
             </div>
-            <Row className='text-center justify-content-center'>
-                <Col lg={6} md={6} sm={12}>
-                    <img className='reviewCircleImg' src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-144251.jpg?w=1380&t=st=1659804907~exp=1659805507~hmac=4148f1b5461d5326b0a21c73ef571c48e7408d8effd6c9143dcc85ce4a7a2f20" alt="" />
-                    <h2 className="reviewName">Noah Lopez</h2>
-                    <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
-                </Col>
+            <Slider {...settings}>
+                <div>
+                    <Row className='text-center justify-content-center'>
+                        <Col lg={6} md={6} sm={12}>
+                            <img className='reviewCircleImg' src="https://img.freepik.com/free-photo/cheerful-indian-businessman-smiling-closeup-portrait-jobs-career-campaign_53876-129417.jpg?w=1480&t=st=1659838535~exp=1659839135~hmac=98662e4a5107cb7db62126585da6cef2b29de6fa22230e326805cb683572647d" alt="" />
+                            <h2 className="reviewName">Noah Lopez</h2>
+                            <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
+                        </Col>
+                    </Row>
+                </div>
 
-            </Row>
+                <div>
+                    <Row className='text-center justify-content-center'>
+                        <Col lg={6} md={6} sm={12}>
+                            <img className='reviewCircleImg' src="https://img.freepik.com/free-photo/portrait-young-pretty-cheerful-girl-smiling_176420-9553.jpg?size=626&ext=jpg&uid=R44347709&ga=GA1.2.1801485300.1659553021" alt="" />
+                            <h2 className="reviewName">Ava Lopez</h2>
+                            <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
+                        </Col>
+                    </Row>
+                </div>
+
+                <div>
+                    <Row className='text-center justify-content-center'>
+                        <Col lg={6} md={6} sm={12}>
+                            <img className='reviewCircleImg' src="https://img.freepik.com/premium-photo/face-happy-young-beautiful-indian-woman-smiling-park-outdoors_251136-24645.jpg?w=740" alt="" />
+                            <h2 className="reviewName">Priscilla Lopez</h2>
+                            <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
+                        </Col>
+                    </Row>
+                </div>
+            </Slider>
         </Container>
       </Fragment>
     )

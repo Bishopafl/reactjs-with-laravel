6981ee591ba381b19c81ca69f4097cbdfdc8e437
Adam Lopez <adamf04@gmail.com>
2022-08-09 11:12:04 -0400

    Found a working typing component plugin, but I am exploring other functionality. This is a working version of typing-effect-reactjs.

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import faceImage from '../../asset/image/face.png';
+import { Typing, TypingStep } from "typing-effect-reactjs";
+
+class AboutMe extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <Container className='text-center'>
+            <h1 className='serviceMainTitle'>My Services</h1>
+            <div className='bottom'></div>
+            <Row>
+                <Col lg={6} md={6} sm={12}>
+                    <div className='aboutMeImage'>
+                        <img className='aboutImg' src={faceImage } alt="" />
+                    </div>
+                </Col>
+
+                <Col lg={6} md={6} sm={12}>
+                    <div className='aboutMeBody'>
+                        <h2 className='aboutMeDetails'>Hi there, I'm</h2>
+                        <h2 className='aboutMeTitle'>Adam Lopez</h2>
+                        <h3 className='aboutMeDetails'>Working as 
+                            <Typing
+                                text={[
+                                    "a Full Stack Web Developer",
+                                    "a Father",
+                                    "a husband",
+                                    "attuned",
+                                ]}
+                                smartBackspace
+                            />
+                            <span id="aboutMeListDetails"></span>
+                        </h3>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default AboutMe

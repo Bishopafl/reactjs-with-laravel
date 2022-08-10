d4ebdd1023bf1131cab5fbebf3a7b67aee9884b0
Adam Lopez <adamf04@gmail.com>
2022-08-10 17:07:47 -0400

    About page is successfully populated with data and is displaying as expected on the front end. This page is ready to consume a backend API.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { React } from 'react';
+import AboutDescription from './components/AboutDescription/AboutDescription';
 import PageTop from './components/PageTop/PageTop';
 import TopNavigation from './components/TopNavigation/TopNavigation';
+import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 
 
 function App() {
   return (
     <div>
-      <TopNavigation />
-      <PageTop pageTitle="About Me" />
+      
+      <AboutPage />
     </div>
   );
 }

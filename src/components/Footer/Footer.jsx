a41c30481c4c3149ea27010def04beb4ca0b8f4e
Adam Lopez <adamf04@gmail.com>
2022-08-10 09:51:14 -0400

    Footer part 2 completed ,changed some of the icons to match my site

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component, Fragment } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends Component {
   render() {
@@ -12,36 +14,37 @@ class Footer extends Component {
       <Fragment>
         <Container fluid={true} className="footerSection">
             <Row>
-                <Col lg={3} md={6} sm={12}>
+                <Col lg={3} md={6} sm={12} className="p-5 text-center">
                     <h2 className='footerName text-center'>Follow Us</h2>
-                    <a href="#">
-                      <FontAwesomeIcon 
-                        icon={ faInstagram }
-                        size='2x'
-                      ></FontAwesomeIcon>
-                    </a>
-                    <a href="#">
-                      <FontAwesomeIcon 
-                        icon={ faDiscord }
-                        size='2x'
-                      ></FontAwesomeIcon>
-                    </a>
-                    <a href="#">
-                      <FontAwesomeIcon 
-                        icon={ faGithub }
-                        size='2x'
-                      ></FontAwesomeIcon>
-                    </a>
-                    <a href="#">
-                      <FontAwesomeIcon 
-                        icon={ faLinkedin }
-                        size='2x'
-                      ></FontAwesomeIcon>
-                    </a>
+                    <div className="socialContainer">
+                      <a href="#" className='github social'>
+                        <FontAwesomeIcon 
+                          icon={ faGithub }
+                          size='2x'
+                        ></FontAwesomeIcon>
+                      </a>
+                      <a href="#" className='discord social'>
+                        <FontAwesomeIcon 
+                          icon={ faDiscord }
+                          size='2x'
+                        ></FontAwesomeIcon>
+                      </a>
+                      <a href="#" className='linkedin social'>
+                        <FontAwesomeIcon 
+                          icon={ faLinkedin }
+                          size='2x'
+                        ></FontAwesomeIcon>
+                      </a>
+                    </div>
                 </Col>
 
                 <Col lg={3} md={6} sm={12}>
                     <h2 className='footerName text-center'>Address</h2>
+                    <p className='footerDescription text-justify'>
+                      <FontAwesomeIcon icon={faMapLocationDot} /> Tampa, Florida <br />
+                      <FontAwesomeIcon icon={faEnvelope} /> Email: adamf04@gmail.com <br />
+                      <FontAwesomeIcon icon={faPhone} /> Phone: 8135008808 <br />
+                    </p>
                 </Col>
 
                 <Col lg={3} md={6} sm={12}>

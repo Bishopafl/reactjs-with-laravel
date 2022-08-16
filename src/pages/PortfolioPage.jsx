8bb36ca54d74f50ddddacda1c7ab1605cf982349
Adam Lopez <adamf04@gmail.com>
2022-08-15 22:37:26 -0400

    adding footer to portfolio page

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import AllProjects from '../components/AllProjects/AllProjects';
 import PageTop from '../components/PageTop/PageTop';
 import TopNavigation from '../components/TopNavigation/TopNavigation';
+import Footer from '../components/Footer/Footer';
 
 class PortfolioPage extends Component {
   render() {
@@ -10,6 +11,7 @@ class PortfolioPage extends Component {
         <TopNavigation title='Our Portfolio' />
         <PageTop pageTitle='Our Portfolio' />
         <AllProjects />
+        <Footer />
       </Fragment>
     )
   }

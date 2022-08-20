331ac9a4e46a912d2f4cee6921bdc01a94cb80f1
Adam Lopez <adamf04@gmail.com>
2022-08-20 15:31:42 -0400

    Project details page is completed, the rest of the data will be coming from the database when the backend is built.

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,19 +1,38 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component, Fragment } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import projectDetailsImage from '../../asset/image/pdetails.png';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+
 class ProjectDetails extends Component {
   render() {
     return (
       <Fragment>
-        <Container>
+        <Container className='mt-5'>
             <Row>
-                <Col lg={6} md={6} sm={12} xs={12}>
-                    <h1>Project Details</h1>
-                    <img src={projectDetailsImage} alt="" />
+                <Col xl={6} lg={6} md={6} sm={12} xs={12}>
+                    <div className="project-details-thumb-wrap after-shape">
+                        <img src={projectDetailsImage} alt="" />
+                    </div>
                 </Col>
 
-                <Col lg={6} md={6} sm={12} xs={12}>
-                    <h1>Project Details</h1>
+                <Col xl={6} lg={6} md={6} sm={12} xs={12} className="mt-5">
+                    <div className='project-details'>
+                        <h1 className='projectDetailsText'>Here are projects that I am most proud of.</h1>
+                        <p className='projectDetailsSubText'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eligendi molestias incidunt rem aliquid reprehenderit corrupti sint ex nisi est voluptatum sapiente accusamus perspiciatis repellendus veniam, blanditiis explicabo, aliquam omnis.</p>
+
+                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> Lorem amet consectetur </p>
+
+                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> Nesciunt mollitia quo maiores</p>
+
+                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> quidem nihil sequi totam </p>
+
+                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> voluptatibus corporis modi odit expedita</p>
+
+                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> facere, adipisci, aliquid veniam fugit</p>
+
+                        <p className="cardSubTitle text-start"><FontAwesomeIcon className='iconBullet' icon={faCheckSquare} /> sint eius sed iure!</p>
+                    </div>
                 </Col>
             </Row>
         </Container>

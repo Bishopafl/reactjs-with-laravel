05d2d86cfc1c3430dc164bc5fea6e09b30faad72
Adam Lopez <adamf04@gmail.com>
2022-08-26 16:10:43 -0400

    Installed and verified axios is returning my API data. much success.

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,7 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
+import axios from 'axios';
 
 class TopBanner extends Component {
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/homedata/title')
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
+
+
   render() {
     return (
       <Fragment>

af0a8d1bb5af17dc69341c0641df0e2f20861845
Adam Lopez <adamf04@gmail.com>
2022-08-28 18:35:41 -0400

    Configured the services section on the homepage to show the information from the database

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,37 +3,53 @@ import { Col, Container, Row } from 'react-bootstrap';
 import cloudnetworkIcon from '../../asset/image/cloud-network.gif';
 import barchartIcon from '../../asset/image/bar-chart.gif';
 import internetIcon from '../../asset/image/internet.gif';
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
 
 class Services extends Component {
+
+  constructor() {
+    super();
+    this.state={
+      serviceData: [],
+    }
+  }
+
+  componentDidMount() {
+    RestClient.GetRequest(AppUrl.Services).then(result => {
+      this.setState({
+        serviceData: result
+      }).catch(error => {
+        this.setState({
+          serviceData: '',
+        })
+      });
+    });
+  }
+
+
   render() {
+
+    const serviceDataList = this.state.serviceData;
+
+    const serviceDataView = serviceDataList.map(serviceDataList => {
+      return <Col lg={4} md={4} sm={12}>
+                <div className='serviceCard text-center'>
+                    <img className='cloudnetworkIcon' src={serviceDataList.service_logo} />
+                    <h2 className='serviceName'>{serviceDataList.service_name}</h2>
+                    <p className='serviceDescription'>{serviceDataList.service_description}</p>
+                </div>
+            </Col>
+    });
+
+
     return (
       <Fragment>
         <Container className='text-center'>
             <h1 className='serviceMainTitle'>My Services</h1>
             <div className='bottom'></div>
-
             <Row>
-                <Col lg={4} md={4} sm={12}>
-                    <div className='serviceCard text-center'>
-                        <img className='cloudnetworkIcon' src={cloudnetworkIcon} />
-                        <h2 className='serviceName'>Web Development</h2>
-                        <p className='serviceDescription'>Developing and deploying web applications via cloud services such as AWS</p>
-                    </div>
-                </Col>
-                <Col lg={4} md={4} sm={12}>
-                    <div className='serviceCard text-center'>
-                        <img className='barchartIcon' src={barchartIcon} />
-                        <h2 className='serviceName'>Increase Engagement</h2>
-                        <p className='serviceDescription'>Increate web engagement and conversion through easy-to-use interfaces</p>
-                    </div>
-                </Col>
-                <Col lg={4} md={4} sm={12}>
-                    <div className='serviceCard text-center'>
-                        <img alt='SecureNetwork' className='internetIcon' src={internetIcon} />
-                        <h2 className='serviceName'>Secure Networks</h2>
-                        <p className='serviceDescription'>Clients recieve immediate up-to-date alerts based on performance of application</p>
-                    </div>
-                </Col>
+                {serviceDataView}
             </Row>
         </Container>
       </Fragment>

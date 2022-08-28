e104f1bb72a87e2773b49b4a010025a256c5816e
Adam Lopez <adamf04@gmail.com>
2022-08-28 16:23:52 -0400

    Homepage title and subtitle returning from the database

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -9,19 +9,21 @@ class TopBanner extends Component {
   constructor() {
     super();
     this.state={
-      title:'',
-      subtitle:'',
+      title:'...',
+      subtitle:'...',
     }
   }
 
   componentDidMount() {
     RestClient.GetRequest(AppUrl.HomeTopTitle)
-    .then(result => {
-      this.setState({
-        title: result[0]['home_title'],
-        subtitle: result[0]['home_subtitle'],
+      .then(result => {
+        this.setState({
+          title: result[0]['home_title'],
+          subtitle: result[0]['home_subtitle'],
+        });
+      }).catch(error => {
+        this.setState({title:'???', subtitle:'????'})
       });
-    });
   }
 
   render() {

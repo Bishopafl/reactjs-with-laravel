0ef1e65fadfb944e7c6d9adfeb5752e7ef862cfe
Adam Lopez <adamf04@gmail.com>
2022-08-29 19:20:10 -0400

    Client reviews are showing from the database dynamically

--- src/components/ClientReview/ClientReview.jsx
+++ src/components/ClientReview/ClientReview.jsx
@@ -3,8 +3,31 @@ import { Col, Container, Row } from 'react-bootstrap';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from 'react-slick';
+import RestClient from '../../RestAPI/RestClient';
+import AppUrl from '../../RestAPI/AppUrl';
 
 class ClientReview extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      clientReviewData: [],
+    }
+  }
+  
+  componentDidMount() {
+    RestClient.GetRequest(AppUrl.ClientReviewsAll).then(result => {
+      this.setState({
+        clientReviewData: result,
+      })
+    }).catch(error => {
+      this.setState({
+        clientReviewData: '',
+      })
+    })
+  }
+  
+  
   render() {
     // only load one slider when component is rendered
     var settings = {
@@ -47,7 +70,19 @@ class ClientReview extends Component {
         ]
       };
 
+      const clientReviewList = this.state.clientReviewData;
 
+      const clientReviewView = clientReviewList.map(clientReviewList => {
+      return <div>
+                  <Row className='text-center justify-content-center'>
+                      <Col lg={6} md={6} sm={12}>
+                          <img className='reviewCircleImg' src={ clientReviewList.client_img } alt="" />
+                          <h2 className="reviewName">{ clientReviewList.client_title }</h2>
+                          <p className="reviewDescription">{ clientReviewList.client_description }</p>
+                      </Col>
+                  </Row>
+             </div>
+      });
 
     return (
       <Fragment>
@@ -57,35 +92,7 @@ class ClientReview extends Component {
                 <div className="reviewBottom "></div>
             </div>
             <Slider {...settings}>
-                <div>
-                    <Row className='text-center justify-content-center'>
-                        <Col lg={6} md={6} sm={12}>
-                            <img className='reviewCircleImg' src="https://img.freepik.com/free-photo/cheerful-indian-businessman-smiling-closeup-portrait-jobs-career-campaign_53876-129417.jpg?w=1480&t=st=1659838535~exp=1659839135~hmac=98662e4a5107cb7db62126585da6cef2b29de6fa22230e326805cb683572647d" alt="" />
-                            <h2 className="reviewName">Noah Lopez</h2>
-                            <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
-                        </Col>
-                    </Row>
-                </div>
-
-                <div>
-                    <Row className='text-center justify-content-center'>
-                        <Col lg={6} md={6} sm={12}>
-                            <img className='reviewCircleImg' src="https://img.freepik.com/free-photo/portrait-young-pretty-cheerful-girl-smiling_176420-9553.jpg?size=626&ext=jpg&uid=R44347709&ga=GA1.2.1801485300.1659553021" alt="" />
-                            <h2 className="reviewName">Ava Lopez</h2>
-                            <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
-                        </Col>
-                    </Row>
-                </div>
-
-                <div>
-                    <Row className='text-center justify-content-center'>
-                        <Col lg={6} md={6} sm={12}>
-                            <img className='reviewCircleImg' src="https://img.freepik.com/premium-photo/face-happy-young-beautiful-indian-woman-smiling-park-outdoors_251136-24645.jpg?w=740" alt="" />
-                            <h2 className="reviewName">Priscilla Lopez</h2>
-                            <p className="reviewDescription">First I would like to thank you for visiting my site today. I have been doing web design and development since October 2015.</p>
-                        </Col>
-                    </Row>
-                </div>
+                { clientReviewView }
             </Slider>
         </Container>
       </Fragment>
